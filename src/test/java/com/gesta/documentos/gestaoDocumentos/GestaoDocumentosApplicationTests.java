dd465d287986c18d6757b7ffff499dfd9e0b0ada
unknown <vp22756@gmail.com>
2025-10-20 19:09:05 -0300

    Primeiro Commit - Gestao de Documentos

---
+++ src/test/java/com/gesta/documentos/gestaoDocumentos/GestaoDocumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gesta.documentos.gestaoDocumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDocumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
